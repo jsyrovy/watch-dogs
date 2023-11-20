28719a45e7afed39c948a65cfb394af82051844c
Jiri Syrovy <jiri.syrovy@firma.seznam.cz>
2023-11-20 18:20:15 +0100

    Remove LaettDog from project

--- dogs/__init__.py
+++ dogs/__init__.py
@@ -1,5 +1,4 @@
-from dogs.ikea.laett import LaettDog
 from dogs.ikea.trofast import TrofastDog
 from dogs.mpo_matrace.viola_natur import ViolaNaturDog
 
-DOGS = (LaettDog, TrofastDog, ViolaNaturDog)
+DOGS = (TrofastDog, ViolaNaturDog)
