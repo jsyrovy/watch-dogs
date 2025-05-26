04c0640b494eee8ed4bf328cde6f31352bc1d3bc
Jiri Syrovy <jiri.syrovy@firma.seznam.cz>
2025-05-26 21:53:35 +0200

    Use vak dog

--- dogs/__init__.py
+++ dogs/__init__.py
@@ -1,3 +1,4 @@
+from dogs.decathlon.vodotesny_vak_30 import VodotesnyVak30
 from dogs.dog import Dog
 
-DOGS: tuple[type[Dog], ...] = ()
+DOGS: tuple[type[Dog], ...] = (VodotesnyVak30,)
--- dogs/decathlon/dog.py
+++ dogs/decathlon/dog.py
@@ -8,7 +8,10 @@ class DecathlonDog(PriceDog):
     URL = "https://www.decathlon.cz/cs/ajax/nfs/prices/online?skuIds="
 
     def _get_value(self) -> int:
-        response = requests.get(f"{self.URL}{self.SKU}", timeout=5)
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",  # noqa: E501
+        }
+        response = requests.get(f"{self.URL}{self.SKU}", timeout=5, headers=headers)
         response.raise_for_status()
         content: dict[str, dict[str, int]] = response.json()
         return content[self.SKU]["price"]
