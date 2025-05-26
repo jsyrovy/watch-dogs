7aa8ddfadada238ccb6defd5d4e2ca150439ef61
Jiri Syrovy <jiri.syrovy@firma.seznam.cz>
2025-05-26 20:56:44 +0200

    Fix mypy errors

--- dogs/ikea/dog.py
+++ dogs/ikea/dog.py
@@ -8,4 +8,6 @@ class IkeaDog(PriceDog):
     def _get_value(self) -> int:
         content = requests.get(self.URL, timeout=5).text
         soup = BeautifulSoup(content, "html.parser")
-        return int(soup.find("span", class_="pip-temp-price__integer").text.replace(" ", ""))
+        if not (price_element := soup.find("span", class_="pip-temp-price__integer")):
+            raise ValueError("Price element not found")
+        return int(price_element.text.replace(" ", ""))
--- dogs/mpo_matrace/dog.py
+++ dogs/mpo_matrace/dog.py
@@ -8,4 +8,6 @@ class MpoMatraceDog(PriceDog):
     def _get_value(self) -> int:
         content = requests.get(self.URL, timeout=5).text
         soup = BeautifulSoup(content, "html.parser")
-        return int(soup.find("span", id="our_price_display").text.replace("Kč", "").replace(" ", ""))
+        if not (price_element := soup.find("span", id="our_price_display")):
+            raise ValueError("Price element not found")
+        return int(price_element.text.replace("Kč", "").replace(" ", ""))
