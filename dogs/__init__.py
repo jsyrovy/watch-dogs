92b7651fe7714d1d475ac5a241fc0d3cdd3f064f
Jiri Syrovy <jiri.syrovy@firma.seznam.cz>
2025-06-25 23:27:10 +0200

    Add Spectral

--- dogs/__init__.py
+++ dogs/__init__.py
@@ -1,5 +1,6 @@
 from dogs.canyon.neuron6 import Neuron6Dog
 from dogs.canyon.neuron_cf7 import NeuronCF7Dog
+from dogs.canyon.spectral_cf7 import SpectralCF7Dog
 from dogs.dog import Dog
 
-DOGS: tuple[type[Dog], ...] = (Neuron6Dog, NeuronCF7Dog)
+DOGS: tuple[type[Dog], ...] = (Neuron6Dog, NeuronCF7Dog, SpectralCF7Dog)
---
+++ dogs/canyon/spectral_cf7.py
@@ -0,0 +1,7 @@
+from dogs.canyon.dog import CanyonDog
+
+
+class SpectralCF7Dog(CanyonDog):
+    URL = "https://www.canyon.com/cs-cz/horska-kola/trailova-kola/spectral/cf/spectral-cf-7/3637.html"
+    NAME = "Spectral CF 7"
+    PRICE = 80_001
