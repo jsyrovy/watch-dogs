805885e695908f75f4ed9ce1448c145786e7004e
Jiri Syrovy <jiri.syrovy@firma.seznam.cz>
2025-06-26 13:10:20 +0200

    Add Spectral 6

---
+++ dogs/canyon/spectral6.py
@@ -0,0 +1,7 @@
+from dogs.canyon.dog import CanyonDog
+
+
+class Spectral6Dog(CanyonDog):
+    URL = "https://www.canyon.com/cs-cz/horska-kola/trailova-kola/spectral/al/spectral-6/4012.html"
+    NAME = "Spectral 6"
+    PRICE = 70_001
