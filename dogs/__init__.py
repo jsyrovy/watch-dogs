65279efaae6c35fc8fa13a92fbd1489375a31e0f
Jiri Syrovy <jiri.syrovy@firma.seznam.cz>
2025-07-27 20:50:18 +0200

    Remove Neurons

--- dogs/__init__.py
+++ dogs/__init__.py
@@ -1,7 +1,5 @@
-from dogs.canyon.neuron6 import Neuron6Dog
-from dogs.canyon.neuron_cf7 import NeuronCF7Dog
 from dogs.canyon.spectral6 import Spectral6Dog
 from dogs.canyon.spectral_cf7 import SpectralCF7Dog
 from dogs.dog import Dog
 
-DOGS: tuple[type[Dog], ...] = (Neuron6Dog, NeuronCF7Dog, Spectral6Dog, SpectralCF7Dog)
+DOGS: tuple[type[Dog], ...] = (Spectral6Dog, SpectralCF7Dog)
